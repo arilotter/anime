5688731fa4488ad7deb77733ab549bacbed4e7c4
Tomasz Dysinski <oz@bunnybones.com>
2019-03-07 23:15:29 -0800

    3.1.0

--- src/index.js
+++ src/index.js
@@ -1268,7 +1268,7 @@ function timeline(params = {}) {
   return tl;
 }
 
-anime.version = '3.0.2';
+anime.version = '3.1.0';
 anime.speed = 1;
 anime.running = activeInstances;
 anime.remove = removeTargets;
